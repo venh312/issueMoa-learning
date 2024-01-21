e44a13c6a3528578852c738f38ea5465120bd5b5
hnev <gpfm312@gmail.com>
2024-01-21 14:42:05 +0900

    [Modify] 인터뷰 조건절 추가 (카테고리)

--- src/main/java/com/issuemoa/learning/service/interview/InterviewService.java
+++ src/main/java/com/issuemoa/learning/service/interview/InterviewService.java
@@ -21,14 +21,8 @@ public class InterviewService {
     private final JPAQueryFactory jpaQueryFactory;
     private final QInterview interview = QInterview.interview;
 
-    public HashMap<String, Object> findAll(HttpServletRequest httpServletRequest, Integer offset, Integer limit) throws JsonProcessingException {
+    public HashMap<String, Object> findAll(String category) throws JsonProcessingException {
         HashMap<String, Object> resultMap = new HashMap<>();
-
-        Long userId = null;
-
-        // 로그인 상태 라면 본인의 학습 진도를 위해 설정
-        HashMap<String, Object> userInfo = usersRestApi.getUserInfo(httpServletRequest);
-        if (userInfo != null) userId = ((Long) userInfo.get("id"));
 
         List<interviewResponse> list = jpaQueryFactory
             .select(Projections.constructor(interviewResponse.class,
@@ -38,38 +32,11 @@ public class InterviewService {
                 interview.answer
             ))
             .from(interview)
-//                .where(
-//                    JPAExpressions.selectFrom(interviewLearn)
-//                        .where(interviewLearn.interviewId.eq(interview.id)
-//                            .and(interviewLearn.learnYn.eq("Y")
-//                            .and(eqId(userId))))
-//                        .notExists()
-//                )
-            .offset(offset)
-            .limit(limit)
+            .where(interview.category.eq(category))
             .orderBy(interview.id.asc())
             .fetch();
 
-        long totalCnt = (long) jpaQueryFactory
-            .select(interview.count())
-            .from(interview)
-//            .where(
-//                JPAExpressions.selectFrom(interviewLearn)
-//                    .where(interviewLearn.interviewId.eq(interview.id)
-//                    .and(interviewLearn.learnYn.eq("Y")
-//                            .and(eqId(userId))))
-//                .notExists()
-//            )
-            .fetchOne();
-
-        int totalPage = (int) Math.ceil((float) totalCnt / limit);
-        totalPage = totalPage == 0 ? 1 : totalPage;
-
         resultMap.put("list", list);
-        resultMap.put("offset", offset);
-        resultMap.put("limit", limit);
-        resultMap.put("totalCnt", totalCnt);
-        resultMap.put("totalPage", totalPage);
 
         return resultMap;
     }
