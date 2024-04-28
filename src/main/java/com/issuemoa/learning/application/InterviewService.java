bca6aed68d28f831bf0832a8c3eab49690b44df5
venh <conf312@naver.com>
2024-04-28 14:22:11 +0900

    [+] 인터뷰 수정 API 개발

--- src/main/java/com/issuemoa/learning/application/InterviewService.java
+++ src/main/java/com/issuemoa/learning/application/InterviewService.java
@@ -1,12 +1,16 @@
 package com.issuemoa.learning.application;
 
+import com.issuemoa.learning.domain.interview.Interview;
+import com.issuemoa.learning.domain.interview.InterviewRepository;
 import com.issuemoa.learning.domain.interview.QInterview;
-import com.issuemoa.learning.presentation.dto.interviewResponse;
+import com.issuemoa.learning.presentation.dto.InterviewRequest;
+import com.issuemoa.learning.presentation.dto.InterviewResponse;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.List;
 
@@ -14,14 +18,15 @@ import java.util.List;
 @Service
 public class InterviewService {
     private final JPAQueryFactory jpaQueryFactory;
+    private final InterviewRepository interviewRepository;
     private final QInterview interview = QInterview.interview;
 
     @Cacheable(value = "interview", key = "#category", cacheManager = "contentCacheManager")
     public HashMap<String, Object> findAll(String category){
         HashMap<String, Object> resultMap = new HashMap<>();
 
-        List<interviewResponse> list = jpaQueryFactory
-            .select(Projections.constructor(interviewResponse.class,
+        List<InterviewResponse> list = jpaQueryFactory
+            .select(Projections.constructor(InterviewResponse.class,
                 interview.id,
                 interview.category,
                 interview.question,
@@ -35,5 +40,15 @@ public class InterviewService {
         resultMap.put("list", list);
 
         return resultMap;
+    }
+
+    @Transactional
+    public InterviewResponse update(InterviewRequest request) {
+        Interview interview = interviewRepository.findById(request.id())
+                                        .orElseThrow(() -> new IllegalArgumentException("[Interview] Not Found ::" + request.id()));
+
+        interview.update(request.category(), request.question(), request.answer(), request.modifyId());
+
+        return new InterviewResponse(interview.getId(), interview.getCategory(), interview.getQuestion(), interview.getAnswer());
     }
 }
