289e6605901566ccd64d2fd95345a8f0e1dd292b
venh <conf312@naver.com>
2025-02-02 19:56:34 +0900

    [+] resolveToken 처리 변경

--- src/main/java/com/issuemoa/learning/presentation/jwt/TokenProvider.java
+++ src/main/java/com/issuemoa/learning/presentation/jwt/TokenProvider.java
@@ -43,7 +43,7 @@ public class TokenProvider {
 
         if (parseToken.isEmpty()) return 0L;
 
-        Claims claims = getClaims(token);
+        Claims claims = getClaims(parseToken);
         return ((Number) claims.get("id")).longValue();
     }
 }
