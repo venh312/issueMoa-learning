0ffe0b4415c5c19597a179f56bc12794c5a95ad5
hnev <gpfm312@gmail.com>
2025-02-08 13:51:19 +0900

    [+] 컨트롤러, JWT 토큰 관련 리팩토링

--- src/main/java/com/issuemoa/learning/presentation/controller/GradeExpController.java
+++ src/main/java/com/issuemoa/learning/presentation/controller/GradeExpController.java
@@ -3,10 +3,6 @@ package com.issuemoa.learning.presentation.controller;
 import com.issuemoa.learning.application.GradeExpService;
 import com.issuemoa.learning.presentation.dto.GradeExpResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,18 +20,12 @@ import java.util.List;
 public class GradeExpController {
     private final GradeExpService gradeExpService;
 
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "조회 성공",
-            content = @Content(schema = @Schema(implementation = GradeExpResponse.class)))})
     @Operation(summary = "Grade Exp 목록", description = "등급 달성 목록을 불러온다.")
     @GetMapping("/grade-exp")
     public ResponseEntity<List<GradeExpResponse>> findAll(){
         return ResponseEntity.ok(gradeExpService.findAll());
     }
 
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "조회 성공",
-            content = @Content(schema = @Schema(implementation = GradeExpResponse.class)))})
     @Operation(summary = "Grade Exp 조회(standard)", description = "standard 에 맞는 등급 경험치 정보를 불러온다.")
     @GetMapping("/grade-exp/{standard}")
     public ResponseEntity<GradeExpResponse> findTop1ByStandardLessThanEqualOrderByStandardDesc(@PathVariable("standard") int standard){
--- src/main/java/com/issuemoa/learning/presentation/controller/InterviewFavoritesController.java
+++ src/main/java/com/issuemoa/learning/presentation/controller/InterviewFavoritesController.java
@@ -2,16 +2,12 @@ package com.issuemoa.learning.presentation.controller;
 
 import com.issuemoa.learning.application.InterviewFavoritesService;
 import com.issuemoa.learning.presentation.dto.InterviewFavoritesRequest;
-import com.issuemoa.learning.presentation.dto.InterviewFavoritesResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Map;
 
@@ -21,22 +17,17 @@ import java.util.Map;
 public class InterviewFavoritesController {
     private final InterviewFavoritesService interviewFavoritesService;
 
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "인터뷰 관심 목록 조회 성공",
-                content = @Content(schema = @Schema(implementation = InterviewFavoritesResponse.class)))})
     @Operation(summary = "인터뷰 관심 목록", description = "사용 여부(useYn) 이 Y인 인터뷰 관심 목록을 조회한다.")
     @GetMapping("/favorites")
-    public ResponseEntity<Map> findByRegisterId(@RequestHeader("Authorization") String token) {
-        return ResponseEntity.ok(interviewFavoritesService.findByRegisterId(token));
+    public ResponseEntity<Map> findByRegisterId(HttpServletRequest request) {
+        return ResponseEntity.ok(interviewFavoritesService.findByRegisterId(request));
     }
 
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "인터뷰 관심 등록/해제 성공")})
     @Operation(summary = "인터뷰 관심 등록/해제", description = "인터뷰 관심 등록/해제한다. (이미 등록되어 있을 경우 사용 여부(useYn)를 변경한다.")
     @PostMapping("/favorites")
     public ResponseEntity<Long> findByRegisterId(
-                @RequestHeader("Authorization") String token,
-                @Valid @RequestBody InterviewFavoritesRequest request) {
-        return ResponseEntity.ok(interviewFavoritesService.save(token, request));
+            HttpServletRequest request,
+                @Valid @RequestBody InterviewFavoritesRequest interviewFavoritesRequest) {
+        return ResponseEntity.ok(interviewFavoritesService.save(request, interviewFavoritesRequest));
     }
 }
